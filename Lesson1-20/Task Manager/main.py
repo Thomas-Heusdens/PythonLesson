897914755d7147be600878b18319267ec9f54835
ThomasHeusdens <thomasheusdens@gmail.com>
2026-02-10 22:09:10 +0100

    Lesson 12

--- Lesson1-20/Task Manager/main.py
+++ Lesson1-20/Task Manager/main.py
@@ -1,7 +1,11 @@
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
-        todos_local = file_local.readlines()
+def get_todos(filepath):
+    with open(filepath, 'r') as file_get:
+        todos_local = file_get.readlines()
     return todos_local
+
+def post_todos(filepath, user_todos):
+    with open(filepath, 'w') as file_post:
+        file_post.writelines(user_todos)
 
 while True:
     action = input("Enter add, show, edit, delete, or exit:")
@@ -9,14 +13,13 @@ while True:
     if action.startswith('add'):
         todo = action[4:]
 
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         todos.append(todo + '\n')
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        post_todos('todos.txt', todos)
     elif action.startswith('show') or action.startswith('display'):
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -29,13 +32,12 @@ while True:
             number = int(action[5:])
             number -= 1
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
 
             new_todo = input("Whats the new todo:")
             todos[number] = new_todo + "\n"
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            post_todos('todos.txt', todos)
         except ValueError:
             print("Your command is not valid. Please use a number to refer to the todo you want to edit.")
             continue
@@ -45,12 +47,11 @@ while True:
             number = int(action[9:])
             number -= 1
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
             old_todo = todos[number].strip("\n")
             todos.remove(todos[number])
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            post_todos('todos.txt', todos)
             print(f"Todo '{old_todo}' was removed from the list of todos.")
         except IndexError:
             print("There is no item with that number.")
