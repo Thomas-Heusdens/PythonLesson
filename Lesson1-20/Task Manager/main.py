8ed003efddb5b22cb687b7c4682d3a3b59ef7d17
ThomasHeusdens <thomasheusdens@gmail.com>
2026-02-13 16:48:38 +0100

    Lesson 13 done

--- Lesson1-20/Task Manager/main.py
+++ Lesson1-20/Task Manager/main.py
@@ -1,9 +1,10 @@
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
+    """Read the todos text file and return the to-do items."""
     with open(filepath, 'r') as file_get:
         todos_local = file_get.readlines()
     return todos_local
 
-def post_todos(filepath, user_todos):
+def post_todos(user_todos, filepath='todos.txt'):
     with open(filepath, 'w') as file_post:
         file_post.writelines(user_todos)
 
@@ -13,13 +14,13 @@ while True:
     if action.startswith('add'):
         todo = action[4:]
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        post_todos('todos.txt', todos)
+        post_todos(todos)
     elif action.startswith('show') or action.startswith('display'):
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -32,12 +33,12 @@ while True:
             number = int(action[5:])
             number -= 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input("Whats the new todo:")
             todos[number] = new_todo + "\n"
 
-            post_todos('todos.txt', todos)
+            post_todos(todos)
         except ValueError:
             print("Your command is not valid. Please use a number to refer to the todo you want to edit.")
             continue
@@ -47,11 +48,11 @@ while True:
             number = int(action[9:])
             number -= 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
             old_todo = todos[number].strip("\n")
             todos.remove(todos[number])
 
-            post_todos('todos.txt', todos)
+            post_todos(todos)
             print(f"Todo '{old_todo}' was removed from the list of todos.")
         except IndexError:
             print("There is no item with that number.")
