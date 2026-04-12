757b541b214986f47d4947b4d94224dc953aab50
ThomasHeusdens <thomasheusdens@gmail.com>
2026-04-12 17:51:23 +0200

    Lesson 26 to 28 done
    Created a weather API with multiple URL possibilities

---
+++ Lesson26-28/WeatherAPI/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+stations = pd.read_csv("../../JupyterLab/data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=stations.to_html())
+
+@app.route("/api/<station>/<date>")
+def base_api(station, date):
+    filename = f"../../JupyterLab/data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {"station": station, "date": date, "temperature": temperature}
+
+@app.route("/api/<station>")
+def station_api(station):
+    filename = f"../../JupyterLab/data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route("/api/year/<station>/<year>")
+def year_api(station, year):
+    filename = f"../../JupyterLab/data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True)
